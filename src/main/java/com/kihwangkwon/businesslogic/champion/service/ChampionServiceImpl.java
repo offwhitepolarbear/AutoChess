4314074c349cadf87725573a159c855abb25b986
offwhitepolarbear <kgh0618@hanmail.net>
2021-04-25 21:56:56 +0900

    compile error commit

--- src/main/java/com/kihwangkwon/businesslogic/champion/service/ChampionServiceImpl.java
+++ src/main/java/com/kihwangkwon/businesslogic/champion/service/ChampionServiceImpl.java
@@ -1,34 +1,30 @@
 package com.kihwangkwon.businesslogic.champion.service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
-import org.h2.util.json.JSONArray;
-import org.h2.util.json.JSONObject;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
 import com.kihwangkwon.domain.db.Champion;
 @Service
 public class ChampionServiceImpl implements ChampionService {
 
 	@Override
 	public int addChampion(Champion champion) {
+		/*
 		String jsonPath = "/file/current_set/champions.json";
-		System.out.println("호출됨");
 			JSONParser jsonParser;
 				InputStream is = getClass().getResourceAsStream(jsonPath);
 				Reader reader = new InputStreamReader(is);
 				jsonParser = new JSONParser(reader);
-				JSONArray jsonObject;
+				Gson gson = new Gson();
+				gson.fromJson(json, classOfT)
+				//jsonObject;
 				try {
 					List<Object> list = jsonParser.parseArray();
 					System.out.println(list);
@@ -36,7 +32,7 @@ public class ChampionServiceImpl implements ChampionService {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+			*/	
 		return 0;
 	}
 
